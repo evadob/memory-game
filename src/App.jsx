1c223e1b95f8a1a7bc3291b93a3d1fc37bb782ed
Eva Dobružská (evadob) <eva.dobruzska@gmail.com>
2023-05-21 18:34:26 +0200

    first version

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import OneCard from "./components/OneCard/OneCard";
+
+const cardImages = [
+  { src: "img/card-1.png", matched: false },
+  { src: "img/card-2.png", matched: false },
+  { src: "img/card-3.png", matched: false },
+  { src: "img/card-4.png", matched: false },
+  { src: "img/card-5.png", matched: false },
+  { src: "img/card-6.png", matched: false },
+];
+
+function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  // check if the choices are a match
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetChoices();
+      } else {
+        setTimeout(() => resetChoices(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  // console.log(cards);
+
+  // reset the choices and increase the turns
+  const resetChoices = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
+  };
+
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Memory Game</h1>
+
+      <button className="btn-new" onClick={shuffleCards}>
+        New Game
+      </button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <OneCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+
+      <p className="p-turns">Turns: {turns}</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/OneCard/OneCard.jsx
@@ -0,0 +1,23 @@
+import "./OneCard.css";
+
+export default function OneCard({ card, handleChoice, flipped, disabled }) {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="card front" />
+        <img
+          className="back"
+          onClick={handleClick}
+          src="/img/cover.png"
+          alt="card back"
+        />
+      </div>
+    </div>
+  );
+}

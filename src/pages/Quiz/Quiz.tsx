55ebf136b18859ec54f4bc7732af22908d727d25
Eva Dobružská <109676559+evadob@users.noreply.github.com>
2023-06-10 18:05:09 +0200

    feat: answers (#6)

--- src/pages/Quiz/Quiz.tsx
+++ src/pages/Quiz/Quiz.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import quizQuestions from "../../../public/data/quizQuestions.json";
 import {
   QuizParams,
@@ -6,9 +6,12 @@ import {
 } from "../../models/quiz";
 import { AnswerResult } from "../AnswerResult/AnswerResult";
 import { QuizQuestion } from "../QuizQuestion/QuizQuestion";
+import { useState } from "react";
 
 export const Quiz = () => {
-  const { questionId } = useParams<QuizParams>();
+  const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
+  const { questionId, questionSetId } = useParams<QuizParams>();
+  const navigate = useNavigate();
 
   if (!questionId) {
     throw Error("Missing question id");
@@ -18,10 +21,24 @@ export const Quiz = () => {
     (question: QuizQuestionModel) => question.id === +questionId
   );
 
-  return (
-    <>
-      <QuizQuestion question={question} />
-      {/* <AnswerResult isCorrect={false} question={question} /> */}
-    </>
+  const handleNextQuestionClick = () => {
+    setIsCorrect(null);
+    const nextQuestionId = +questionId + 1;
+    navigate(`/quiz/${questionSetId}/${nextQuestionId}`);
+  };
+
+  const handleAnswerClick = (answerIndex: number) => {
+    const result = question?.correctAnswer === answerIndex;
+    setIsCorrect(result);
+  };
+
+  return isCorrect === null ? (
+    <QuizQuestion question={question} handleAnswerClick={handleAnswerClick} />
+  ) : (
+    <AnswerResult
+      isCorrect={isCorrect}
+      question={question}
+      handleNextQuestionClick={handleNextQuestionClick}
+    />
   );
 };
